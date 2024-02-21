9de116f2b17b2cdecc8758fd5f1ff9fa6b574397
vanhvanh <vanhleg2301@gmail.com>
2024-02-21 09:41:05 +0700

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
+function Header() {
+  return (
+    <div>
+      <h1>This is header </h1>
+    </div>
+  );
+}

f5e72c042b376f773569a46b75fd3b828776526f
= <vanhleg2301@gmail.com>
2024-03-03 21:04:05 +0700

    database

--- src/admin/pages/Categorylist.js
+++
@@ -1,9 +0,0 @@
-const Categorylist = () => {
-  return (
-    <div>
-      <h3 className="mt-2">Product Categories</h3>
-    </div>
-  );
-};
-
-export default Categorylist;
--- src/components/CartContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react';
-
-// Create and export a CartContext
-export const CartContext = createContext();

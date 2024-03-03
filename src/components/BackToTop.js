5a38e0ad902b24f4c9718fd36ed8a83c1c78cc10
= <vanhleg2301@gmail.com>
2024-03-03 20:52:33 +0700

    database

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -6,28 +6,46 @@ const BackToTop = () => {
   const [backToTop, setBackToTop] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.screenY > 100) {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
         setBackToTop(true);
       } else {
         setBackToTop(false);
       }
-    });
-  });
+    };
 
-  const top = () => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
+
   return (
-    <div>
-      {backToTop && (
-        <button className="topbtn" onClick={top}>
-          Top
-        </button>
-      )}
+    <div
+      className="back-to-top"
+      style={{
+        display: backToTop ? "block" : "none",
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        cursor: "pointer",
+        backgroundColor: "#007bff",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        zIndex: 9999,
+      }}
+      onClick={scrollToTop}
+    >
+      Back to Top
     </div>
   );
 };
